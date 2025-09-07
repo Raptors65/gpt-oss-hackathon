1f414589fa841aba84394a4f2b15773dfe7211a1
Lucas Kim <lucaskamadakim@gmail.com>
2025-09-06 23:19:50 -0400

    Add larger list of learning domains

--- backend/config.example.py
+++ backend/config.example.py
@@ -1,2 +1,74 @@
 NOTES_DIR = "/Users/abcd/notes"
 SQLITE_FILE_NAME = "database.db"
+LEARNING_DOMAINS = [
+    # General knowledge / encyclopedias
+    "wikipedia.org",
+    "britannica.com",
+    "scholarpedia.org",
+    "infoplease.com",
+
+    # Open courseware / universities
+    "ocw.mit.edu",
+    "coursera.org",
+    "edx.org",
+    "khanacademy.org",
+    "udacity.com",
+    "udemy.com",
+    "futurelearn.com",
+    "openlearn.open.ac.uk",
+    "classcentral.com",
+
+    # Technical documentation / programming
+    "docs.python.org",
+    "developer.mozilla.org",
+    "readthedocs.io",
+    "w3schools.com",
+    "geeksforgeeks.org",
+    "cplusplus.com",
+    "cppreference.com",
+    "rust-lang.org",
+    "golang.org",
+    "docs.oracle.com",
+    "kubernetes.io",
+    "tensorflow.org",
+    "pytorch.org",
+
+    # Research / science
+    "arxiv.org",
+    "ncbi.nlm.nih.gov",
+    "nature.com",
+    "sciencedirect.com",
+    "plos.org",
+    "springer.com",
+
+    # Q&A / knowledge sharing
+    "stackoverflow.com",
+    "stackexchange.com",
+    "quora.com",
+    "mathoverflow.net",
+    "reddit.com/r/askscience",
+    "reddit.com/r/explainlikeimfive",
+
+    # Learning communities / blogs
+    "medium.com",
+    "dev.to",
+    "towardsdatascience.com",
+    "freecodecamp.org",
+    "codecademy.com",
+    "tutorialspoint.com",
+    "learncpp.com",
+    "sqlbolt.com",
+
+    # Dictionaries / references
+    "dictionary.com",
+    "merriam-webster.com",
+    "thesaurus.com",
+    "etymonline.com",
+
+    # Government & open data (educational)
+    "data.gov",
+    "census.gov",
+    "nasa.gov",
+    "who.int",
+    "worldbank.org"
+]
--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from openai import AsyncOpenAI
 from agents import set_default_openai_client
-from config import NOTES_DIR
+from config import LEARNING_DOMAINS, NOTES_DIR
 from notes.updater import NotesUpdater
 from notes.resources import Webpage
 from practice.updater import get_questions_for_note
@@ -38,7 +38,7 @@ app.add_middleware(
 async def add_website(url: str):
     """Adds a website to the notes."""
 
-    if "wikipedia.org" not in url:
+    if all(domain in url for domain in LEARNING_DOMAINS):
         return
 
     await updater.add_update(Webpage(url))
